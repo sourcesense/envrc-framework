fc8a4f2a8ae9d000b371aeafcef80d39332b6652
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2021-02-25 11:22:29 +0100

    Added bootstrap.sh

---
+++ _bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export _DEP_VERBOSENESS_LEVEL=0
+
+# shellcheck source=/_dep-bootstrap.sh
+. "$(fetch_url https://raw.githubusercontent.com/EcoMind/envrc-framework/main/_dep-bootstrap.sh sha256-lOYbrk89hNgXowWn1q17tpqUeNnEXJLyDTl7mLhbcpU=)" 0.5.1
+
+dep define "log2/shell-common:0.3.2"
+dep define "EcoMind/k8s-common:0.2.3"
+
+dep include log2/shell-common strings
+dep include log2/shell-common log
+dep include log2/shell-common files
+dep include log2/shell-common calc
