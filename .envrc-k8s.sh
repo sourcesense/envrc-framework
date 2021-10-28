3e9d75282aadbef117727f8bffada429587cae5d
Antonio Fragola <a.fragola@eco-mind.eu>
2021-10-28 17:04:45 +0200

    switched .envrc-k8s.sh to tagged version

--- .envrc-k8s.sh
+++ .envrc-k8s.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck source=/_bootstrap.sh
-source_url "https://raw.githubusercontent.com/EcoMind/envrc-framework/main/_bootstrap.sh" "sha256-3GOXXaPhsm8gnkH1B+K0IcTxckjC1Pclap5Xps/VnOE="
+source_url "https://raw.githubusercontent.com/EcoMind/envrc-framework/v0.1.0/_bootstrap.sh" "sha256-//rB0lHUGHwlUtDxhsUM88PXoBQQgPywMXaZd4Aevbk="
 
 work_on() {
     local release_name="$1"
