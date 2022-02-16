dd99bbdc5a63e22e952a0a8f2829a5e620b629ca
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2022-02-16 10:05:10 +0100

    Switched to centralized dep-bootstrap 0.5.1

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -3,7 +3,7 @@
 export _DEP_VERBOSENESS_LEVEL=0
 
 # shellcheck source=/_dep-bootstrap.sh
-. "$(fetchurl "https://raw.githubusercontent.com/EcoMind/envrc-framework/v0.13.0/_dep-bootstrap.sh" "sha256-lOYbrk89hNgXowWn1q17tpqUeNnEXJLyDTl7mLhbcpU=")" 0.5.1
+. "$(fetchurl "https://raw.githubusercontent.com/EcoMind/dep-bootstrap/0.5.1/dep-bootstrap.sh" "sha256-lOYbrk89hNgXowWn1q17tpqUeNnEXJLyDTl7mLhbcpU=")" 0.5.1
 
 dep define "log2/shell-common:0.5.2"
 dep define "EcoMind/k8s-common:0.2.14"
