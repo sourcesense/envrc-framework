f68be7dc91b525cf79179722e245290eb38f4d72
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2022-02-16 10:13:23 +0100

    Added requirements for k9s, kustomize and sops
    k9s 0.25.18
    kustomize 4.5.2
    sops 3.7.1

--- .envrc-k8s.sh
+++ .envrc-k8s.sh
@@ -3,6 +3,10 @@
 # shellcheck source=/_bootstrap.sh
 source_url "https://raw.githubusercontent.com/EcoMind/envrc-framework/v0.15.0/_bootstrap.sh" "sha256-P_8dq3yMs0euE3omgVgSkfvzCHp7EfBfRYmYUJW5l_w="
 
+req_ver k9s 0.25.18
+req_ver kustomize 4.5.2
+req_ver sops 3.7.1
+
 work_on() {
     local release_name="$1"
     local namespace_name="${2:-${release_name}}"
