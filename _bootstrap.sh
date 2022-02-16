bceb6f351740585f85a763e5168539343de306b8
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2022-02-16 10:04:38 +0100

    Bumped k8s-common 0.2.13 -> 0.2.14

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -6,7 +6,7 @@ export _DEP_VERBOSENESS_LEVEL=0
 . "$(fetchurl "https://raw.githubusercontent.com/EcoMind/envrc-framework/v0.13.0/_dep-bootstrap.sh" "sha256-lOYbrk89hNgXowWn1q17tpqUeNnEXJLyDTl7mLhbcpU=")" 0.5.1
 
 dep define "log2/shell-common:0.5.2"
-dep define "EcoMind/k8s-common:0.2.13"
+dep define "EcoMind/k8s-common:0.2.14"
 
 dep include log2/shell-common strings
 dep include log2/shell-common log
