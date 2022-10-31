d181c551c7e8f0b1aef2c008dbd0a275de4c8639
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2022-10-31 16:43:59 +0100

    Bump k8s-common 0.2.20 -> 0.2.22
    In turn, this bumps awscli 2.7.7 -> 2.8.7

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -6,7 +6,7 @@ export _DEP_VERBOSENESS_LEVEL=0
 . "$(fetchurl "https://raw.githubusercontent.com/EcoMind/dep-bootstrap/0.5.1/dep-bootstrap.sh" "sha256-lOYbrk89hNgXowWn1q17tpqUeNnEXJLyDTl7mLhbcpU=")" 0.5.1
 
 dep define "log2/shell-common:0.5.9"
-dep define "EcoMind/k8s-common:0.2.20"
+dep define "EcoMind/k8s-common:0.2.22"
 
 dep include log2/shell-common strings
 dep include log2/shell-common log
